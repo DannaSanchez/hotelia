d7c829e986f38f111e323fca736a6215e27bac8f
liseth Bohorquez <sanchezdanna15@gmail.com>
2022-06-28 15:14:55 -0500

    routes, create folders in components

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Login from './pages/Login';
+import PerfilUsuario from './pages/PerfilUsuario';
+import RegistroHuesped from './pages/RegistroHuesped';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login/>}></Route>
+      <Route path='/Form/huesped' element={<RegistroHuesped/>}></Route>
+      <Route path='/editarperfil' element={<PerfilUsuario/>}></Route>
     </Routes>
   </BrowserRouter>
 );
