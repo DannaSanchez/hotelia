383c9efc1816723f54da0325bb37f74dfa97be6e
Angela Diaz <angeldi846@gmail.com>
2022-08-02 22:11:13 -0500

    Update Login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,20 +1,14 @@
 import React, {useState} from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik'
-
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import Logo2 from '../../img/Hotelia horizontal negro.png';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
-function Login (){
+function Login (usuarios){
 
     const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
-
-    //const [shown, setShown] = React.useState(false);
-
-    //const switchShown = () => setShown(!shown);
-
-    //const [password, setPassword] = React.useState('');
-    //const onChange = ({ currentTarget }) => setPassword(currentTarget.value);
 
     return(
         <>
@@ -42,15 +36,25 @@ function Login (){
                     } else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.correo)){
                         errores.correo = 'Correo electrónico no válido'
                     }
-    
                     return errores;
                 }}
     
                 onSubmit={(valores, {resetForm}) => {
-                    resetForm();
-                    console.log('Formulario enviado');
-                    cambiarFormularioEnviado(true);
-                    setTimeout(() => window.location.href="/listado-habitaciones-admin", 50);
+                        if((valores.correo==="Carlosrr@gmail.com" && valores.password==="JUAN1245") || (valores.correo==="cambio@gmail.com" && valores.password==="cambio123")){
+                            resetForm();
+                            console.log('Formulario enviado');
+                            cambiarFormularioEnviado(true);
+                            setTimeout(() => window.location.href="/listado-habitaciones-admin", 50);}
+                    else{
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Usuario no registrado',
+                            confirmButtonColor: "#9C2759",
+                            background: '#FFFDFB',
+                            color: '#000'
+                        })
+                        console.log('Usuario no registrado');
+                    }
                 }}
             >
 
@@ -70,43 +74,21 @@ function Login (){
                             type='text' 
                             id='correo' 
                             name='correo' 
-
                             className="login__input"
                         />
-
                         <ErrorMessage name='correo' component={() => (<div className='login__error'>{errors.correo}</div>)}/>
-
                     </div>
-
                     <div className='login__form-group'>
                         <label htmlFor="password" className='login__label'>Contraseña</label>
                         <Field 
-                           
                             type='password'
-
                             id='password' 
                             name='password' 
-
                             className="login__input"
-
-                            
                         />
-
-                        {/*
-                        
-                        onChange={onChange}
-                            type={shown ? 'text' : 'password'} 
-                        value={password}
-
-                        <button onClick={switchShown}>
-                            {shown ? `Ocultar` : `Mostrar`}
-                        </button>*/}
-
                         <ErrorMessage name='password' component={() => (<div className='login__error'>{errors.password}</div>)}/>
 
                     </div>
-                    
-                    {/*<Boton link="/listado-habitaciones-admin" buttonid="login__button" description="Ingresar"/>*/}
 
                     <button type='submit' className='login__button'>Enviar</button>
 
