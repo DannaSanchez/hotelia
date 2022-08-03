1d63ebabba6677bc74acfdae494cb38bd13c8e08
Danna Sanchez <sanchezdanna15@gmail.com>
2022-08-02 23:26:20 -0500

    add github page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Inicio from './pages/Inicio';
 import Login from './pages/Login';
@@ -24,11 +24,15 @@ root.render(
       <Route path="/listado-habitaciones-admin" element={<ListadoHabitacion />}></Route>
       <Route path="/modificar-habitaciones-admin" element={<ModificarHabitacion />}></Route>
       <Route path="/registrar-habitaciones-admin" element={<RegistrarHabitacion />}></Route>
-      <Route path="/" element={<Login />}></Route>
+      {/*<Route path="/" element={<Login />}></Route>*/}
       <Route path='/form/huesped' element={<RegistroHuesped />}></Route>
       <Route path='/editarperfil' element={<PerfilUsuario />}></Route>
       <Route path="/habitaciones-huesped" element={<Habitaciones />}></Route>
       <Route path='/reservas-huesped' element={<ReservaRealizada />}></Route>
+      <Route path="/hotelia/" element={<Navigate replace to= {"/"}/>}></Route>
+      <Route path="/hotelia" element={<Navigate replace to= {"/"}/>}></Route>
+      <Route path="*" element={<Navigate replace to= {"/"}/>}></Route>
+      
     </Routes>
   </BrowserRouter>
 );
